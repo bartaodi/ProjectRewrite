9bfcbe12683c43930af9d0cb09071a65f5d1b267
bartaodi <barta.kod@gmail.com>
2021-05-13 15:24:14 +0200

    max function implemented

--- src/main/java/ProjectRewrite.java
+++ src/main/java/ProjectRewrite.java
@@ -1,7 +1,6 @@
 public class ProjectRewrite {
     public static void main(String[] args) {
-        int[] maxArr = {1,2,3,4,5};
-        System.out.println(max(maxArr));
+        System.out.println(multiply(0, 5));
     }
 
     private static int min(int x, int y) {
@@ -26,9 +25,34 @@ public class ProjectRewrite {
         return max;
     }
 
-    private static int length(int[] values) { return 0; }
+    private static int length(int[] values) {
+        int count = 0;
+        for (int value : values) {
+            count++;
+        }
+        return count;
+    }
 
-    private static int multiply(int x, int y) { return 0; }
+    private static int multiply(int x, int y) {
+        int result = 0;
+        boolean isNegative = false;
+
+        if (x < 0) {
+            x = -x;
+            isNegative = true;
+        }
+
+        if (y < 0) {
+            y = -y;
+            isNegative = !isNegative;
+        }
+
+        for (int i = 0; i < y; i++) {
+            result = result + x;
+        }
+
+        return isNegative ? -result : result;
+    }
 
     private static int pow(int x, int y) { return 0; }
 
