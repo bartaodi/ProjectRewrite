fd26b52fa88338392b10cf2fbf9be6c897463a0a
bartaodi <barta.kod@gmail.com>
2021-05-13 14:01:19 +0200

    setup project

---
+++ src/main/java/ProjectRewrite.java
@@ -0,0 +1,17 @@
+public class ProjectRewrite {
+    public static void main(String[] args) {
+
+    }
+
+    private static int min(int x, int y) { return 0; }
+
+    private static int max(int[] values) { return 0; }
+
+    private static int length(int[] values) { return 0; }
+
+    private static int multiply(int x, int y) { return 0; }
+
+    private static int pow(int x, int y) { return 0; }
+
+    private static int[] divmod(int x, int y) { return new int[0]; }
+}
